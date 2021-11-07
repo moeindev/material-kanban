031807b62345693122f5793bf56e0e51f6ca561d
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-11-07 20:41:33 +0330

    fix card styles & show boards

--- src/app/components/boards/boards.component.ts
+++ src/app/components/boards/boards.component.ts
@@ -1,26 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Board} from "../../models/board";
 import {BoardItemOperations} from "../board-item/BoardItemOperations";
+import {BoardsServiceService} from "../../services/boards-service.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
   styleUrls: ['./boards.component.scss']
 })
-export class BoardsComponent implements OnInit, BoardItemOperations {
+export class BoardsComponent implements OnInit, BoardItemOperations, OnDestroy {
 
-  constructor() { }
+  boards: Board[] = [];
+
+  boardsSubscription?: Subscription;
+
+  constructor(private boardService: BoardsServiceService) {
+  }
 
   ngOnInit(): void {
+    this.boardsSubscription = this.boardService.boards().subscribe((boards) => {
+      this.boards = boards;
+    });
+  }
+
+  ngOnDestroy() {
+    this.boardsSubscription?.unsubscribe()
   }
 
   onUpdate(board: Board) {
+    console.log(board)
   }
 
   onDelete(board: Board) {
+    console.log(board)
   }
 
   onSelect(board: Board) {
+    console.log(board)
   }
 
 }
