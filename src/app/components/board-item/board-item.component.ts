8cc705d4ed0977346dacc41d10b23a1169269667
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-11-07 19:36:53 +0330

    Design board items

--- src/app/components/board-item/board-item.component.ts
+++ src/app/components/board-item/board-item.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Board} from "../../models/board";
 
 @Component({
   selector: 'app-board-item',
   templateUrl: './board-item.component.html',
   styleUrls: ['./board-item.component.scss']
 })
-export class BoardItemComponent implements OnInit {
+export class BoardItemComponent {
+
+  @Input()
+  board!: Board;
+
+  @Output() deleteBoard: EventEmitter<Board> = new EventEmitter<Board>();
+  @Output() updateBoard: EventEmitter<Board> = new EventEmitter<Board>();
+  @Output() selectBoard: EventEmitter<Board> = new EventEmitter<Board>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  onDelete(board: Board) {
+    this.deleteBoard.emit(board);
+  }
+
+  onUpdate(board: Board) {
+    this.updateBoard.emit(board);
+  }
+
+  onSelect(board: Board) {
+    this.selectBoard.emit(board);
   }
 
 }
