9a4165958c04cdea9d434ed8d87cfef607f3144f
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-11-07 14:50:51 +0330

    change theme

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  headerTitle: String = 'Material kanban Board';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onHamMenu() {
+    
+  }
 }
